6bd4f50d20a6015d8ac93b0c390209287f8ea862
Patrick Durasiewicz <pdurasie@pm.me>
2019-10-09 17:11:39 +0200

    Feature: Have dynamic circle shaped color indicators

--- app/src/main/java/com/example/android/earthquakeudacity/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/earthquakeudacity/EarthquakeAdapter.java
@@ -1,15 +1,19 @@
 package com.example.android.earthquakeudacity;
 
 import android.app.Activity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.graphics.drawable.GradientDrawable;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.text.SimpleDateFormat;
@@ -32,10 +36,14 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         Earthquake currentEarthquake = getItem(position);
 
         TextView mag = listItemView.findViewById(R.id.mag_text_view);
-        mag.setText(String.valueOf(currentEarthquake.getMagnitude()));
+        mag.setText(formatMagnitude(currentEarthquake.getMagnitude()));
+        GradientDrawable magnitudeCircle = (GradientDrawable) mag.getBackground();
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+        magnitudeCircle.setColor(magnitudeColor);
 
-        TextView location = listItemView.findViewById(R.id.location_text_view);
-        location.setText(currentEarthquake.getLocation());
+        TextView nearTextView = listItemView.findViewById(R.id.near_text_view);
+        TextView locationTextView = listItemView.findViewById(R.id.location_text_view);
+        setLocation(currentEarthquake.getLocation(), nearTextView, locationTextView);
 
         Date dateObject = new Date(currentEarthquake.getTimeInMilliseconds());
 
@@ -64,5 +72,57 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
     private String formatTime(Date dateObject) {
         SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
         return timeFormat.format(dateObject);
+    }
+
+    private String formatMagnitude(double magnitude){
+        DecimalFormat magnitudeFormat = new DecimalFormat("0.0");
+        return magnitudeFormat.format(magnitude);
+    }
+
+    private int getMagnitudeColor(double magnitude){
+        int magnitudeRounded = (int) Math.floor(magnitude);
+        int color;
+        switch(magnitudeRounded){
+            case 0:
+            case 1: color = R.color.magnitude1;
+                    break;
+            case 2: color = R.color.magnitude2;
+                    break;
+            case 3: color = R.color.magnitude3;
+                    break;
+            case 4: color = R.color.magnitude4;
+                    break;
+            case 5: color = R.color.magnitude5;
+                    break;
+            case 6: color = R.color.magnitude6;
+                    break;
+            case 7: color = R.color.magnitude7;
+                    break;
+            case 8: color = R.color.magnitude8;
+                    break;
+            case 9: color = R.color.magnitude9;
+                    break;
+            case 10: color = R.color.magnitude10plus;
+                    break;
+            default: color = R.color.magnitude1;
+                    break;
+        }
+        return ContextCompat.getColor(getContext(), color);
+    }
+
+    private void setLocation(String initialLocation,
+                             TextView nearTextView,
+                             TextView locationTextView){
+
+        if (initialLocation.contains("km") && initialLocation.contains("of")){
+            //(?<=) is called a positive lookaround in regular expressions
+            //it makes the "of" be included in the split string
+            String [] splitString = initialLocation.split("(?<=of)");
+            nearTextView.setText(splitString[0]);
+            locationTextView.setText(splitString[1]);
+        } else{
+            nearTextView.setText(R.string.near_of);
+            locationTextView.setText(initialLocation);
+        }
     }
 }
