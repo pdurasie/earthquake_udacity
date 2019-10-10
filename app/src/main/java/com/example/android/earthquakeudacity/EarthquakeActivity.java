bd03aeec1318a384306e62bb29329e82890d4d43
Patrick Durasiewicz <pdurasie@pm.me>
2019-10-10 15:53:37 +0200

    Feature: Add HttpRequest for real time data

--- app/src/main/java/com/example/android/earthquakeudacity/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/earthquakeudacity/EarthquakeActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,28 +16,37 @@ import android.widget.ListView;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.Buffer;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
-
+    private EarthquakeAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        ArrayList<Earthquake> earthquakes = QueryUtils.extractEarthquakes();
-
-        // Find a reference to the {@link ListView} in the layout
         final ListView earthquakeListView = findViewById(R.id.list);
-
         // Create a new {@link ArrayAdapter} of earthquakes
-        EarthquakeAdapter adapter = new EarthquakeAdapter(this, earthquakes);
+        mAdapter = new EarthquakeAdapter(this, new ArrayList<Earthquake>());
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
-        earthquakeListView.setAdapter(adapter);
+        earthquakeListView.setAdapter(mAdapter);
+
+        EarthquakeAsyncTask task = new EarthquakeAsyncTask();
+        task.execute();
 
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -47,5 +58,92 @@ public class EarthquakeActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void updateUi(List<Earthquake> earthquakes){
+        mAdapter.clear();
+        if(earthquakes != null && !earthquakes.isEmpty()){
+            mAdapter.addAll(earthquakes);
+        }
+    }
+
+    private class EarthquakeAsyncTask extends AsyncTask<String, Void, List<Earthquake>> {
+        @Override
+        protected List<Earthquake> doInBackground(String... strings) {
+
+            URL url = createUrl(QueryUtils.SAMPLE_JSON_RESPONSE);
+            String jsonResponse = "";
+
+            try {
+                jsonResponse = makeHttpRequest(url);
+            } catch (IOException e){
+                Log.e(LOG_TAG, "IOException thrown", e);
+            }
+            return QueryUtils.extractEarthquakes(jsonResponse);
+        }
+
+        @Override
+        protected void onPostExecute(List<Earthquake> earthquakes) {
+            updateUi(earthquakes);
+        }
+
+        private URL createUrl(String stringUrl){
+            URL url = null;
+            try {
+                url = new URL(stringUrl);
+            } catch (MalformedURLException e) {
+                Log.e(LOG_TAG, "IOException thrown", e);
+            }
+            return url;
+        }
+
+        private String makeHttpRequest(URL url) throws IOException{
+            String jsonResponse = "";
+            if(url == null){
+                return jsonResponse;
+            }
+
+            HttpURLConnection urlConnection = null;
+            InputStream inputStream = null;
+            try{
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(15000);
+                urlConnection.connect();
+                if(urlConnection.getResponseCode() == 200){
+                    inputStream = urlConnection.getInputStream();
+                    jsonResponse = readFromStream(inputStream);
+                }else{
+                    Log.e(LOG_TAG, "Response code is not 200" );
+                }
+            }catch (IOException e){
+                Log.e(LOG_TAG, "IOException thrown", e);
+            } finally{
+                //close everything if they existed
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if(inputStream != null){
+                    inputStream.close();
+                }
+            }
+            return jsonResponse;
+        }
+
+        private String readFromStream(InputStream inputStream) throws IOException{
+            StringBuilder output = new StringBuilder();
+            if (inputStream != null){
+                InputStreamReader streamReader =
+                        new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                BufferedReader reader = new BufferedReader(streamReader);
+                String line = reader.readLine();
+                while(line != null){
+                    output.append(line);
+                    line = reader.readLine();
+                }
+            }
+            return output.toString();
+        }
     }
 }
